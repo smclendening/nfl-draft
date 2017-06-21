eb31c3e731144d76c723e04be737945c87dc166e
Scott <smclendening@gmail.com>
2017-06-20 21:51:05 -0700

    Begin knex configuration

--- db/config.js
+++ db/config.js
@@ -1,12 +1,32 @@
-const pgp = require('pg-promise')();
+const knex = require('./knexfile.js');
 
-const db = pgp('postgres://localhost:5432/nfldraft');
+const db = require('knex')({
+  client: 'pg',
+  connection: knex.development.connection,
+  pool: {
+    afterCreate: (conn, done) => {
+      console.log('hmm are we in here');
+      conn.query('SET timezone="UTC";', err => {
+        if (err) {
+          done(err, conn);
+        } else {
+          conn.query('SELECT set_limit(0.01);', err => {
+            done(err, conn);
+          })
+        }
+      })
+    }
+  }
+})
 
-module.exports = db.connect()
-    .then(obj => {
-      console.log('successfully connected');
-        obj.done(); // success, release the connection;
-    })
-    .catch(error => {
-        console.log('ERROR:', error.message || error);
-    });
+// const db = pgp('postgres://localhost:5432/nfldraft');
+
+// module.exports = db.connect()
+//     .then(obj => {
+//       console.log('successfully connected');
+//         obj.done(); // success, release the connection;
+//     })
+//     .catch(error => {
+//         console.log('ERROR:', error.message || error);
+//     });
+
---
+++ db/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost:5432/nfldraft',
+    migrations: {
+      directory: __dirname + '/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/seeds/development'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
