cf3fdc4a9e7869a979d0bdc182cdd78f578def83
Scott <smclendening@gmail.com>
2017-07-26 22:43:24 -0700

    Change margins of drop downs

--- client/src/components/ChartOptions.jsx
+++ client/src/components/ChartOptions.jsx
@@ -3,55 +3,77 @@ import { connect } from 'react-redux';
 import { allTeams, allPositions } from '../utils.js';
 
 
-const labelStyle = { width: '200px', float: 'left', margin: '0 20px 20px' };
+const labelStyle = { width: '200px', float: 'left', margin: '0 5px 20px' };
 const spanStyle = { display: 'block', margin: '0 0 3px', font: 'Verdana', 'fontSize': '14px' };
-const inputStyle = { width: '200px', padding: '5px', height: '30px' };
+const inputStyle = { width: '160px', padding: '2px', height: '30px' };
 
-export default ({ props }) => (
-  <div
-    className="ui one column stackable center aligned page grid"
-    style={{ marginTop: '5px' }}
-  >
-    <label style={labelStyle}>
-      <span style={spanStyle}>Workout/Attribute:</span>
-      <select
-        className="ui fluid search dropdown"
-        style={inputStyle}
-        // TODO: add onChange event to each select
-      >
-        <option className="item" value="forty_yd">40-Yard Dash</option>
-        <option className="item" value="vertical">Vertical Jump</option>
-        <option className="item" value="bench_reps">Bench Press</option>
-        <option className="item" value="cone">Three-Cone</option>
-        <option className="item" value="shuttle">Shuttle Run</option>
-        <option className="item" value="vertical">Vertical</option>
-        <option className="item" value="height">Height</option>
-        <option className="item" value="weight">Weight</option>
-      </select>
-    </label>
+export default class ChartOptions extends Component {
 
-    <label style={labelStyle}>
-      <span style={spanStyle}>Team:</span>
-      <select
-        className="ui fluid search dropdown"
-        style={inputStyle}
-      >
-        {allTeams.map(team => (
-          <option className="item" value={team}>{team}</option>
-        ))}
-      </select>
-    </label>
+  constructor() {
+    super();
+    this.state = {
+      workout: 'forty_yd',
+      team: 'Arizona Cardinals',
+      position: 'QB'
+    }
+  }
 
-    <label style={labelStyle}>
-      <span style={spanStyle}>Position:</span>
-      <select
-        className="ui fluid search dropdown"
-        style={inputStyle}
+  render() {
+    console.log(this.state.workout)
+    return (
+      <div
+        className="ui one column stackable center aligned page grid"
+        style={{ marginTop: '5px' }}
       >
-        {allPositions.map(position => (
-          <option className="item" value={position}>{position}</option>
-        ))}
-      </select>
-    </label>
-  </div>
-)
+        <label style={labelStyle}>
+          <span style={spanStyle}>Workout/Attribute:</span>
+          <select
+            className="ui fluid search dropdown"
+            style={inputStyle}
+            onChange={(e) => this.setState({workout: e.target.value})}
+          >
+            <option className="item" value="forty_yd">40-Yard Dash</option>
+            <option className="item" value="vertical">Vertical Jump</option>
+            <option className="item" value="bench_reps">Bench Press</option>
+            <option className="item" value="cone">Three-Cone</option>
+            <option className="item" value="shuttle">Shuttle Run</option>
+            <option className="item" value="vertical">Vertical</option>
+            <option className="item" value="height">Height</option>
+            <option className="item" value="weight">Weight</option>
+          </select>
+        </label>
+
+        <label style={labelStyle}>
+          <span style={spanStyle}>Team:</span>
+          <select
+            className="ui fluid search dropdown"
+            style={inputStyle}
+            onChange={(e) => this.setState({team: e.target.value})}
+          >
+            {allTeams.map(team => (
+              <option className="item" value={team}>{team}</option>
+            ))}
+          </select>
+        </label>
+
+        <label style={labelStyle}>
+          <span style={spanStyle}>Position:</span>
+          <select
+            className="ui fluid search dropdown"
+            style={inputStyle}
+            onChange={(e) => this.setState({team: e.target.value})}
+          >
+            {allPositions.map(position => (
+              <option className="item" value={position}>{position}</option>
+            ))}
+          </select>
+        </label>
+
+        <label style={labelStyle}>
+          <button className="ui black button">Get Player Data</button>
+        </label>
+
+      </div>
+    )
+  }
+}
