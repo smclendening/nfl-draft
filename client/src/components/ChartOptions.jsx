16b48b9de2162040c09d061a3e9c744a95988822
Scott <smclendening@gmail.com>
2017-07-26 21:40:25 -0700

    Store all teams in utils for drop down selection

--- client/src/components/ChartOptions.jsx
+++ client/src/components/ChartOptions.jsx
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { allTeams } from '../utils.js';
 
 
 const labelStyle = { width: '200px', float: 'left', margin: '0 20px 20px' };
-const spanStyle = { display: 'block', margin: '0 0 3px', font: 'lato', 'fontSize': '14px' };
+const spanStyle = { display: 'block', margin: '0 0 3px', font: 'Verdana', 'fontSize': '14px' };
 const inputStyle = { width: '200px', padding: '5px', height: '30px' };
 
-export default ({ props }) => (
+export default ({ props }) => {
+  console.log(allTeams);
+return (
   <div
     className="ui one column stackable center aligned page grid"
     style={{ marginTop: '5px' }}
   >
     <label style={labelStyle}>
-      <span style={spanStyle}>Workout:</span>
+      <span style={spanStyle}>Workout/Attribute:</span>
+      <select
+        className="ui fluid search dropdown"
+        style={inputStyle}
+        // TODO: add onChange event to each select
+      >
+        <option className="item" value="forty_yd">40-Yard Dash</option>
+        <option className="item" value="vertical">Vertical Jump</option>
+        <option className="item" value="bench_reps">Bench Press</option>
+        <option className="item" value="cone">Three-Cone</option>
+        <option className="item" value="shuttle">Shuttle Run</option>
+        <option className="item" value="vertical">Vertical</option>
+        <option className="item" value="height">Height</option>
+        <option className="item" value="weight">Weight</option>
+      </select>
+    </label>
+
+    <label style={labelStyle}>
+      <span style={spanStyle}>Team:</span>
       <select
         className="ui fluid search dropdown"
         style={inputStyle}
       >
-        <option className="item" value="0">40-yard Dash</option>
-        <option className="item" value="1">Bench Press</option>
+        {allTeams.map(team => (
+          <option className="item" value={team}>{team}</option>
+        ))}
       </select>
     </label>
   </div>
 )
+}
---
+++ client/src/utils.js
@@ -0,0 +1,10 @@
+export const allTeams = ['Arizona Cardinals', 'Atlanta Falcons', 'Baltimore Ravens', 'Buffalo Bills', 
+'Carolina Panthers', 'Chicago Bears', 'Cincinnati Bengals', 'Cleveland Browns', 
+'Dallas Cowboys', 'Denver Broncos', 'Detroit Lions', 'Green Bay Packers',
+'Houston Texans', 'Indianapolis Colts', 'Jacksonville Jaguars', 'Kansas City Chiefs', 
+'Miami Dolphins', 'Minnesota Vikings', 'New England Patriots', 'New Orleans Saints', 
+'New York Giants', 'New York Jets', 'Oakland Raiders', 'Philadelphia Eagles', 'Pittsburgh Steelers', 
+'San Diego Chargers', 'San Francisco 49ers', 'Seattle Seahaws', 'St. Louis Rams', 
+'Tampa Bay Buccaneers', 'Tennessee Titans', 'Washington Redskins'];
+
+export const allPositions = ['QB', '']
