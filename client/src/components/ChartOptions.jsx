d4295de71b3cd889e7d2614c7303fbc80980927f
Scott <smclendening@gmail.com>
2017-08-07 22:19:14 -0700

    Add WorkoutOptions component

--- client/src/components/ChartOptions.jsx
+++ client/src/components/ChartOptions.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ChartDropdown from './ChartDropdown.jsx';
+import WorkoutOptions from './WorkoutOptions.jsx';
 
 export default () => (
   <div>
+    <WorkoutOptions />
     <ChartDropdown />
   </div>
 )
---
+++ client/src/components/WorkoutOptions.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class WorkoutOptions extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
