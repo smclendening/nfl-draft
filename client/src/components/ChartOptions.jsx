b7c9eb7a77975756585d1669ad2b96f50bee6033
Scott <smclendening@gmail.com>
2017-08-07 22:12:30 -0700

    Make ChartOptions a presentational stateless component

--- client/src/components/ChartOptions.jsx
+++ client/src/components/ChartOptions.jsx
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ChartDropdown from './ChartDropdown.jsx';
 
-export default class ChartOptions extends Component {
-  render() {
-    return (
-      <ChartDropdown />
-    )
-  }
-}
+export default () => (
+  <div>
+    <ChartDropdown />
+  </div>
+)
