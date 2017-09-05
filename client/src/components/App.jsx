7726c163806f75d048a674bcb78628a89d7b5100
Scott <smclendening@gmail.com>
2017-09-04 17:14:46 -0700

    Remove inline styling from App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,14 +3,11 @@ import React, { Component, PropTypes } from 'react';
 import Nav from './Nav.jsx';
 import ChartHome from './BarChart/ChartHome.jsx';
 
-const gridStyle = {
-  marginTop: '64px'
-}
 
 const App = () => (
   <div>
     <Nav />
-    <div style={gridStyle}>
+    <div className="ChartHome">
       <ChartHome />
     </div>
   </div>
