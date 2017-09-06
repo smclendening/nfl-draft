61cbe86efd2a53e274303f856ae0f68cb3b8e168
Scott <smclendening@gmail.com>
2017-09-05 20:44:11 -0700

    Remove inline styling from Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,5 @@
 import React from 'react';
 
-const navStyle = {
-  marginBottom: '0',
-  borderRadius: '0',
-  height: '52px',
-  background: '#002347'
-};
-
-const spanStyle = {
-  color: 'white',
-}
-
 const Nav = () => (
   <div id="navbar" className="ui fixed menu">
     <a className="item">
